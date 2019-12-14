3af1065f0201fabcac85c2150cc80177027d9a4e
David Uzumaki <iamdaviduzumaki@gmail.com>
2019-12-14 17:47:31 +0000

    cleanup

--- main.go
+++ main.go
@@ -11,34 +11,12 @@ func main() {
 	bulbs, _ := golifx.LookupBulbs()
 	nameOfBulbAndPowerStatus(bulbs)
 
-	//Turn if off
-	// if !powerState {
-	// 	bulbs[0].SetPowerState(true)
-	// }
-
-	// ticker := time.NewTicker(time.Second)
-	// counter := 0
-
-	// hsbk := &golifx.HSBK{
-	// 	Hue:        2000,
-	// 	Saturation: 13106,
-	// 	Brightness: 65535,
-	// 	Kelvin:     3200,
-	// }
-	// //Change color every second
-	// // for _ = range ticker.C {
-	// // 	bulbs[0].SetColorState(hsbk, 500)
-	// // 	counter++
-	// // 	hsbk.Hue += 5000
-	// // 	if counter > 10 {
-	// // 		ticker.Stop()
-	// // 		break
-	// // 	}
-	// // }
-	// //Turn off
-	// bulbs[0].SetPowerState(false)
 }
 
+/*
+	Description: loop through array of LIFX bulbs and get their name and location.
+	Then print it out.
+*/
 func nameOfBulbAndPowerStatus(bulbs []*golifx.Bulb) {
 	for _, bulb := range bulbs {
 		location, _ := bulb.GetLabel()
